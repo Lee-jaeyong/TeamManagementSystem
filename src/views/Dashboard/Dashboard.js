8234ec776143a505c6d0a77366955e8692a719f0
Lee-jaeyong <wodyd202@naver.com>
2020-04-16 01:38:05 +0900

    @View -- 스케줄러 구현 중

--- src/views/Dashboard/Dashboard.js
+++ src/views/Dashboard/Dashboard.js
@@ -3,16 +3,14 @@ import React,{useEffect} from "react";
 import ChartistGraph from "react-chartist";
 // @material-ui/core
 import { makeStyles } from "@material-ui/core/styles";
+import { CardActionArea } from "@material-ui/core";
 import Icon from "@material-ui/core/Icon";
 // @material-ui/icons
 import Store from "@material-ui/icons/Store";
-import Warning from "@material-ui/icons/Warning";
 import DateRange from "@material-ui/icons/DateRange";
 import LocalOffer from "@material-ui/icons/LocalOffer";
-import Update from "@material-ui/icons/Update";
 import ArrowUpward from "@material-ui/icons/ArrowUpward";
 import AccessTime from "@material-ui/icons/AccessTime";
-import Accessibility from "@material-ui/icons/Accessibility";
 import BugReport from "@material-ui/icons/BugReport";
 import Code from "@material-ui/icons/Code";
 import Cloud from "@material-ui/icons/Cloud";
@@ -20,7 +18,6 @@ import Cloud from "@material-ui/icons/Cloud";
 // core components
 import GridItem from "components/Grid/GridItem.js";
 import GridContainer from "components/Grid/GridContainer.js";
-import Table from "components/Table/Table.js";
 import Tasks from "components/Tasks/Tasks.js";
 import CustomTabs from "components/CustomTabs/CustomTabs.js";
 import Danger from "components/Typography/Danger.js";
@@ -29,8 +26,6 @@ import CardHeader from "components/Card/CardHeader.js";
 import CardIcon from "components/Card/CardIcon.js";
 import CardBody from "components/Card/CardBody.js";
 import CardFooter from "components/Card/CardFooter.js";
-
-
 
 import { bugs, website, server } from "variables/general.js";
 
@@ -49,19 +44,33 @@ export default function Dashboard(props) {
   useEffect(()=>{
   },[props.match.params.idx]);
 
+  const showScheduler = () => {
+    props['history'].push('/admin/scheduler/'+props.match.params.idx);
+  }
+
+  const showReferenceData = () => {
+    props['history'].push('/admin/referenceData/'+props.match.params.idx);
+  }
+
+  const showNotice = () => {
+    props['history'].push('/admin/notice/'+props.match.params.idx);
+  }
+
   return (
     <div>
       <GridContainer>
-        <GridItem xs={12} sm={6} md={3}>
+        <GridItem xs={12} sm={12} md={4}>
           <Card>
+            <CardActionArea onClick={showScheduler}>
             <CardHeader color="warning" stats icon>
               <CardIcon color="warning">
                 <Icon>content_copy</Icon>
               </CardIcon>
-              <p className={classes.cardCategory}>일 정</p>
+              <p className={classes.cardCategory}>Scheduler</p>
               <h3 className={classes.cardTitle}>
-                49/50 <span style={{fontSize:15}}> (건)</span>
+                스케줄러
               </h3>
+              <br/>
             </CardHeader>
             <CardFooter stats>
               <div className={classes.stats}>
@@ -69,67 +78,60 @@ export default function Dashboard(props) {
                   <Store />
                 </Danger>
                 <a href="#pablo" onClick={e => e.preventDefault()}>
-                  내 일정 보기
+                  일정 보기
                 </a>
               </div>
             </CardFooter>
+            </CardActionArea>
           </Card>
         </GridItem>
-        <GridItem xs={12} sm={6} md={3}>
+        <GridItem xs={12} sm={12} md={4}>
           <Card>
+            <CardActionArea onClick={showReferenceData}>
             <CardHeader color="success" stats icon>
               <CardIcon color="success">
                 <Store />
               </CardIcon>
-              <p className={classes.cardCategory}>프로젝트 시작일</p>
-              <h3 className={classes.cardTitle}>2020-04-14</h3>
+              <p className={classes.cardCategory}>ReferenceData</p>
+              <h3 className={classes.cardTitle}>참고자료</h3>
+              <br/>
             </CardHeader>
             <CardFooter stats>
               <div className={classes.stats}>
-                <DateRange />
-                1 일 경과
+                <Danger>
+                  <DateRange />
+                </Danger>
+                1 (건)
               </div>
             </CardFooter>
+            </CardActionArea>
           </Card>
         </GridItem>
-        <GridItem xs={12} sm={6} md={3}>
+        <GridItem xs={12} sm={12} md={4}>
           <Card>
+            <CardActionArea onClick={showNotice}>
             <CardHeader color="danger" stats icon>
               <CardIcon color="danger">
                 <Icon>info_outline</Icon>
               </CardIcon>
-              <p className={classes.cardCategory}>프로젝트 최종일</p>
-              <h3 className={classes.cardTitle}>2020-10-10</h3>
+              <p className={classes.cardCategory}>Notice</p>
+              <h3 className={classes.cardTitle}>공지사항</h3><br/>
             </CardHeader>
             <CardFooter stats>
               <div className={classes.stats}>
-                <LocalOffer />
-                10일 남음
+                <Danger>
+                  <LocalOffer />
+                </Danger>
+                10 (건)
               </div>
             </CardFooter>
-          </Card>
-        </GridItem>
-        <GridItem xs={12} sm={6} md={3}>
-          <Card>
-            <CardHeader color="info" stats icon>
-              <CardIcon color="info">
-                <Accessibility />
-              </CardIcon>
-              <p className={classes.cardCategory}>공지사항</p>
-              <h3 className={classes.cardTitle}>245<span style={{fontSize:15}}> (건)</span></h3>
-            </CardHeader>
-            <CardFooter stats>
-              <div className={classes.stats}>
-                <Update />
-                공지사항 보기
-              </div>
-            </CardFooter>
+            </CardActionArea>
           </Card>
         </GridItem>
       </GridContainer>
       <GridContainer>
         <GridItem xs={12} sm={12} md={6}>
-          <Card chart>
+          <Card>
             <CardHeader color="success">
               <ChartistGraph
                 className="ct-chart"
@@ -168,8 +170,8 @@ export default function Dashboard(props) {
               />
             </CardHeader>
             <CardBody>
-              <h4 className={classes.cardTitle}>일정률</h4>
-              <p className={classes.cardCategory}>팀원과 자신의 일정률을 비교할 수 있습니다.</p>
+              <h4 className={classes.cardTitle}>개인별 진척도</h4>
+              <p className={classes.cardCategory}>팀원과 자신의 진척도를 비교할 수 있습니다.</p>
             </CardBody>
             <CardFooter chart>
               <div className={classes.stats}>
